f93a672d6d8cd992f92a0be44fa091850778e854
kimkangyun <rlarkddbs3@osstem.com>
2023-10-04 10:58:15 +0900

    'init'

---
+++ src/main/java/com/example/kdbe/KdbeApplication.java
@@ -0,0 +1,13 @@
+package com.example.kdbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KdbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kdbe/KdbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kdbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
