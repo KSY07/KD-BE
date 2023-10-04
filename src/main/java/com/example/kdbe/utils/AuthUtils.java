38fb4c03e22b93734231f434bd4f43f30620807d
tandoori <ksy2008w@naver.com>
2023-10-04 19:42:22 +0900

    2023-10-04 - Setting @Value SECRET_KEY

--- src/main/java/com/example/kdbe/utils/AuthUtils.java
+++ src/main/java/com/example/kdbe/utils/AuthUtils.java
@@ -1,8 +1,13 @@
 package com.example.kdbe.utils;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AuthUtils {
+
+    @Value("${auth.secretKey}")
+    private String SECRET_KEY;
+
 
 }
