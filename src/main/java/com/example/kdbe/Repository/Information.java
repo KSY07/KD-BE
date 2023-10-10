42d1cadaf076b2574f75a8127337d0479cf563c1
kimkangyun <rlarkddbs3@osstem.com>
2023-10-10 17:46:17 +0900

    오타 수정

--- src/main/java/com/example/kdbe/Repository/Information.java
+++
@@ -1,6 +0,0 @@
-package com.example.kdbe.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Information extends JpaRepository<Information,Long> {
-}
--- src/main/java/com/example/kdbe/Repository/RoleSpaceRepository.java
+++ src/main/java/com/example/kdbe/Repository/RoleSpaceRepository.java
@@ -1,6 +1,7 @@
 package com.example.kdbe.Repository;
 
+import com.example.kdbe.model.entity.RoleSpace;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RoleSpaceRepository extends JpaRepository<RoleSpaceRepository,Long> {
+public interface RoleSpaceRepository extends JpaRepository<RoleSpace,Long> {
 }
