7c3d3d92fce35c67d0c323cbcb2872d2d222a79c
tandoori <ksy2008w@naver.com>
2023-10-15 17:55:19 +0900

    2023-10-15 - 예외 추가

---
+++ src/main/java/com/example/kdbe/exception/RoleNotFoundException.java
@@ -0,0 +1,9 @@
+package com.example.kdbe.exception;
+
+public class RoleNotFoundException extends Exception{
+
+    public RoleNotFoundException(String message)
+    {
+        super(message);
+    }
+}
