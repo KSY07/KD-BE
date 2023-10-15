ac39674c545f339c344c6009885d4eb20080f1c9
tandoori <ksy2008w@naver.com>
2023-10-15 17:55:42 +0900

    2023-10-15 - 가입 매핑 추가

--- src/main/java/com/example/kdbe/web/UserController.java
+++ src/main/java/com/example/kdbe/web/UserController.java
@@ -4,6 +4,7 @@ package com.example.kdbe.web;
 import com.example.kdbe.auth.KdbeUserDetail;
 import com.example.kdbe.auth.KdbeUserDetailService;
 import com.example.kdbe.model.dto.request.SignInRequestDto;
+import com.example.kdbe.model.dto.request.SignUpRequestDto;
 import com.example.kdbe.model.dto.request.UserRequestDto;
 import com.example.kdbe.model.dto.response.SignInResponseDto;
 import com.example.kdbe.model.entity.User;
@@ -11,6 +12,7 @@ import com.example.kdbe.service.UserService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -30,6 +32,19 @@ public class UserController implements BaseController<User,UserRequestDto> {
         KdbeUserDetail userDetail = userDetailService.loadUserByUsername(req.getUserId());
 
         return service.signIn(req, userDetail);
+    }
+
+    @PostMapping("/signUp")
+    public ResponseEntity<?> signUp(@RequestBody SignUpRequestDto req)
+    {
+        boolean res = service.signUp(req);
+
+        if(!res)
+        {
+            return ResponseEntity.badRequest().body("SignUp Failed");
+        }
+
+        return ResponseEntity.ok(req);
     }
 
 }
