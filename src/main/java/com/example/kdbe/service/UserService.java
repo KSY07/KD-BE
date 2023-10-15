6beff3c7d11eaaabdc968be99d8172236dfd5b97
tandoori <ksy2008w@naver.com>
2023-10-15 17:56:13 +0900

    2023-10-15 - 회원가입, 로그인 메서드 추가

--- src/main/java/com/example/kdbe/service/UserService.java
+++ src/main/java/com/example/kdbe/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.kdbe.service;
 
 import com.example.kdbe.auth.KdbeUserDetail;
 import com.example.kdbe.model.dto.request.SignInRequestDto;
+import com.example.kdbe.model.dto.request.SignUpRequestDto;
 import com.example.kdbe.model.dto.request.UserRequestDto;
 import com.example.kdbe.model.dto.response.SignInResponseDto;
 import com.example.kdbe.model.entity.User;
@@ -10,7 +11,8 @@ import jakarta.transaction.Transactional;
 @Transactional
 public interface UserService extends BaseService<User,UserRequestDto> {
 
-    public SignInResponseDto signIn(SignInRequestDto req, KdbeUserDetail userDetail);
-    public void saveRefToken(String userId, String refToken, String authToken);
-    public void removeRefToken(String authToken);
+    public SignInResponseDto    signIn(SignInRequestDto req, KdbeUserDetail userDetail);
+    public boolean              signUp(SignUpRequestDto req);
+    public void                 saveRefToken(String userId, String refToken, String authToken);
+    public void                 removeRefToken(String authToken);
 }
