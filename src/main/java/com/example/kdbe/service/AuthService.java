65c1b7af5015720eb1e44d8ac5b90d782c6015fb
tandoori <ksy2008w@naver.com>
2023-10-15 17:02:42 +0900

    2023-10-15 - UserService에 통합

--- src/main/java/com/example/kdbe/service/AuthService.java
+++
@@ -1,6 +0,0 @@
-package com.example.kdbe.service;
-
-public interface AuthService {
-    public void saveRefToken(String userId, String refToken, String authToken);
-    public void removeRefToken(String authToken);
-}
